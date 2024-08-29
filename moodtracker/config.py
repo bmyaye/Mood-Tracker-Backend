70ee767c85e3cd9d1f4f932478aef2cc7a5d01e6
Ai Mousika <prxvatex@gmail.com>
2024-08-29 21:49:16 +0700

    about .env file

--- moodtracker/config.py
+++ moodtracker/config.py
@@ -8,6 +8,14 @@ class Settings(BaseSettings):
         env_file=".env", validate_assignment=True, extra="allow"
     )
 
+"""
+
+create file .env in the same level as moodtracker folder
+
+in file .env
+SQLDB_URL=postgresql+asyncpg://postgres:<your_password>@localhost:5432/your_database_name
+
+"""
 
 def get_settings():
     return Settings()
