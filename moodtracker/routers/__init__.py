1160bd1f8295ebf527f83b1e3596e6e7c986f2e6
Chayanin086 <125025419+Chayanin086@users.noreply.github.com>
2024-09-01 04:24:06 +0700

    Update __init__.py

--- moodtracker/routers/__init__.py
+++ moodtracker/routers/__init__.py
@@ -1,6 +1,8 @@
 from . import moods
 from . import users
+from . import authentications
 
 def init_router(app):
-    app.include_router(moods.router)
+    app.include_router(authentications.router)
     app.include_router(users.router)
+    app.include_router(moods.router)
